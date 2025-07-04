55dc3349e1b5c25c2b1929e68cf6b2135b055d60
shour0 <shour908@gmail.com>
2025-07-04 23:15:28 +0300

    feat: add transaction types

--- types/transaction.ts
+++ types/transaction.ts
@@ -7,8 +7,44 @@ export interface Transaction {
   amount: number;
   type: TransactionType;
   category: string;
-  date: Timestamp; 
+  date: Date; 
   updatedAt?: Timestamp;
   description?: string;
   createdAt?: Timestamp;
+}
+
+export type Props = {
+  showAddTransaction: boolean;
+  setShowAddTransaction: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export type FirestoreTimestamp = {
+  _seconds: number;
+  _nanoseconds?: number;
+} | Date | string | number | null | undefined;
+
+export interface ChartDataPoint {
+  date: string;
+  amount: number;
+  type: TransactionType;
+}
+
+export interface FormData {
+  amount: string;
+  type: TransactionType;
+  category: string;
+  date: string;
+  description: string;
+}
+
+export interface PieLabelProps {
+  cx: number;
+  cy: number;
+  midAngle: number;
+  innerRadius: number;
+  outerRadius: number;
+  percent: number;
+  index: number;
+  name: string;
+  value: number;
 }
