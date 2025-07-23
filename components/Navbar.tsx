14c7b47a223fb6e8836d1f5eb6d1790e98ceed73
shour0 <shour908@gmail.com>
2025-07-23 20:42:59 +0300

    fix: make resizable navbar become less smaller when scrolling and remove wrapping in LogOut text

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -158,7 +158,7 @@ const Navbar: React.FC<Props> = ({ showAddTransaction, setShowAddTransaction }) 
                     variant="secondary"
                     className="w-full flex items-center gap-2"
                   >
-                    <LogOut className="h-4 w-4" />
+                    <LogOut className="h-4 w-4 whitespace-nowrap" />
                     Sign Out
                   </NavbarButton>
                 </>
